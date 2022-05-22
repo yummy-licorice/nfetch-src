6596df47659ecf3c162d1ae46e8268c8a1c07ac2
Luke <Infinitybeond1@protonmail.com>
2022-05-22 12:32:02 -0500

    fix init system bug on macos

--- src/init.nim
+++ src/init.nim
@@ -1,7 +1,17 @@
+import distros
+import os
+
 proc getInit*(): string =
-    let init_file = open("/proc/1/comm")
-    let init = init_file.readLine()
-    return init
+    if detectOs(MacOSX):
+        let init = "initd"
+        return init
+    if detectOs(Linux) and fileExists("/proc/1/comm"):
+        let init_file = open("/proc/1/comm")
+        let init = init_file.readLine()
+        return init
+    else:
+        let init = "Cant detect init"
+        return init
 
 
         
