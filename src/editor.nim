b83dbbd073b1e2ca19181c86466c44c5fe96aa4c
theArctesian <stephen.d.okita@gmail.com>
2022-05-22 17:43:18 +0800

    little clean up

--- src/editor.nim
+++ src/editor.nim
@@ -3,7 +3,7 @@ import os
 proc getEditor*(): string =
     let edi = os.getEnv("EDITOR") 
     if edi == "": 
-        return "none"
-        ## return "nvim" ## i am manually changing this cuz i have no env editor
+        ## return "none"
+        return "nvim" ## i am manually changing this cuz i have no env editor
     else: 
         return edi
--- src/nfetch.nim
+++ src/nfetch.nim
@@ -6,7 +6,7 @@ import init
 import std/terminal
 
 stdout.styledWriteLine(fgBlue, "distro: ", fgWhite, getDistro())
-stdout.styledWriteLine(fgMagenta, "arch: ", fgWhite, getArch())
-stdout.styledWriteLine(fgGreen, "init: ", fgWhite, getInit())
-stdout.styledWriteLine(fgRed, "shell: ", fgWhite, getShell())
+stdout.styledWriteLine(fgMagenta, "arch  : ", fgWhite, getArch())
+stdout.styledWriteLine(fgGreen, "init  : ", fgWhite, getInit())
+stdout.styledWriteLine(fgRed, "shell : ", fgWhite, getShell())
 stdout.styledWriteLine(fgYellow, "editor: ", fgWhite, getEditor()) ## this is a personal change because I don't have
